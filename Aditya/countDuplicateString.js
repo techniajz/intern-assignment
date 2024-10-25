642e4b02df17865e1ecca5bcb421e07f943122e1
aditya raj sharma <adityarajsharma777@gmail.com>
2024-10-25 17:13:01 +0530

    added 6 files

---
+++ Aditya/countDuplicateString.js
@@ -0,0 +1,11 @@
+let str = "aaahhhhhhwwjjj";
+let map = new Map();
+for(let i = 0;i<str.length;i++){
+    if(map.has(str[i])){
+        map.set(str[i],(map.get(str[i])+1));
+    }
+    else{
+        map.set(str[i],1);
+    }
+}
+console.log(map);
